f4c5bd457b29b4a3f0e34a9e46d84fdbfa9cbed4
sean-morris <sean.smorris@gmail.com>
2022-08-30 20:39:44 +0000

    week 4

---
+++ StringExamplesMain.java
@@ -0,0 +1,7 @@
+public class StringExampleMain{
+    public static void main(String[] args) {
+        StringExamples s = new StringExamples();
+        s.testIndexOf();
+        //s.testSubstring();
+    } 
+}
